fbcc5226df051f4564a507c892fa91b71cb0e0cc
Tomasz Myszkowski <myszkowskitomek523@gmail.com>
2021-11-29 22:52:27 +0100

    zmiane w formularzu

--- register/views.py
+++ register/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 # Create your views here.
 
 def register(response):
     if response.method == "POST":
-        form = UserCreationForm(response.POST)
+        form = CreateUserForm(response.POST)
         if form.is_valid():
             form.save()
         return  redirect("/home") #do zrobienia home
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
     return render(response, "register/register.html", {"form":form})
