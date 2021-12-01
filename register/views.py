57d715bce1b227693fbed6353b41a4f23a727f91
Tomasz Myszkowski <myszkowskitomek523@gmail.com>
2021-12-01 22:17:05 +0100

    gotowy login rejestracja logout

--- register/views.py
+++ register/views.py
@@ -10,7 +10,7 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect("/home")
+        return redirect("home")
     else:
         form = CreateUserForm()
     return render(request,
