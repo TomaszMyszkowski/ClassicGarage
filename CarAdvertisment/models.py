8b0c7a0c163867682b55df4ce319a86224c23afb
olka <a_lukowska@tlen.pl>
2021-12-04 12:56:36 +0100

    Baza: zmiana prize - na price, relacje

--- CarAdvertisment/models.py
+++ CarAdvertisment/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 status = [
@@ -38,14 +39,14 @@ class car_adveritsment(models.Model):
 
     # id = models.IntegerField(primary_key=True)
     id_car = models.ForeignKey('car', on_delete=models.CASCADE)
-    id_user = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     brand = models.CharField(max_length=15)
     model = models.CharField(max_length=15)
-    year = models.IntegerField() #metoda field.unique_for_year
+    year = models.IntegerField()  #metoda field.unique_for_year
     add_date = models.DateField()
     modified_date = models.DateField()
     end_date = models.DateField()
-    prize = models.IntegerField()
+    price = models.IntegerField()
     status = models.CharField(choices=status, max_length=8)
     location = models.CharField(choices=change_location, max_length=4)
     telephone = models.IntegerField()
@@ -56,18 +57,18 @@ class car_adveritsment(models.Model):
 class part_of_car_advertisment(models.Model):
 
     # id = models.IntegerField(primary_key=True)
-    id_user = models.IntegerField()
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     add_date = models.DateField()
     modified_date = models.DateField()
     end_date = models.DateField()
-    prize = models.IntegerField()
+    price = models.IntegerField()
     status = models.CharField(choices=status, max_length=8)
     location = models.CharField(choices=change_location, max_length=4)
     telephone = models.IntegerField()
     description = models.TextField(max_length=1000)
 
-class service_avertisment(models.Model):
+class service_advertisment(models.Model):
 
     service = [
         ('spraying', 'lakierowanie'),
@@ -76,7 +77,7 @@ class service_avertisment(models.Model):
     ]
 
     # id = models.IntegerField(primary_key=True)
-    id_user = models.IntegerField()
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
     service = models.CharField(choices=service, max_length=12)
     add_date = models.DateField()
     modified_date = models.DateField()
