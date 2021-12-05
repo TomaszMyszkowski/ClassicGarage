a7f69e4e6390f459dea8185cff6362e52c06e39c
TomaszMyszkowski <86147004+TomaszMyszkowski@users.noreply.github.com>
2021-12-05 13:29:10 +0100

    wyswietlanie listy ogłoszen

--- PartsAdvertisment/views.py
+++ PartsAdvertisment/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, redirect, get_object_or_404
 from PartsAdvertisment.forms import PartsAdvert
 from CarAdvertisment.models import PartOfCarAdvertisment
 
+
 # Create your views here.
 
 def part_add(request):
-
     form = PartsAdvert(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('advert')
+        advert_id = PartOfCarAdvertisment.objects.order_by("id").last().id
+        return redirect('partadvert', id=advert_id)
     return render(request,
                   'part-add.html',
                   context={
@@ -17,8 +18,9 @@ def part_add(request):
                   }
                   )
 
-def part_advert(request, user):
-    ad = get_object_or_404(PartOfCarAdvertisment, user=user)
+
+def part_advert(request, id):
+    ad = get_object_or_404(PartOfCarAdvertisment, id=id)
 
     return render(request,
                   'partadvertview.html',
